1d36eeea3c1ce46965f8ba5aae9cd05730f5826b
Leo de Oz <astaroth.satania@gmail.com>
2019-03-29 11:42:30 -0500

    mas cambios

--- calquipa_reportemexicanos_parte1_it/__init__.py
+++ calquipa_reportemexicanos_parte1_it/__init__.py
@@ -26,4 +26,4 @@ import reporte_consolidado_resultado
 import reporte_pre_flujo_efectivo
 import reporte_flujo_efectivo
 import reporte_consolidado_flujo_efectivo
-reporte_mexicano_extraccion_usd
+import reporte_mexicano_extraccion_usd
